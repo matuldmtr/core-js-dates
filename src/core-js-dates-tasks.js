864720e402dd3e3ed9674b47f84128cf94114f3f
Dmitry Matul <matuldmtr@gmail.com>
2024-03-23 13:38:16 +0100

    feat: getCountDaysOnPeriod complete

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -107,8 +107,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const startInMS = Date.parse(dateStart);
+  const endInMS = Date.parse(dateEnd);
+
+  return (endInMS - startInMS) / 1000 / 3600 / 24 + 1;
 }
 
 /**
